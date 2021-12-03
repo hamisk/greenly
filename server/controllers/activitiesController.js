0ec877598ccd43cbbe1e076d1cbca61dbb6df8d0
hamisk <hs.khera@icloud.com>
2021-12-02 21:52:05 -0800

    structure backend activity data

--- server/controllers/activitiesController.js
+++ server/controllers/activitiesController.js
@@ -1,13 +1,46 @@
 const knex = require('knex')(require('../knexfile').development);
 
 exports.getActivities = (_req, res) => {
-  knex('activities')
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) =>
-      res.status(400).send(`Error retrieving activities: ${err}`)
-    );
-};
+    knex('activities')
+        .then((data) => {
+            // console.log(data)
+            data.sort((a,b) => a.id - b.id)
+            let prevActivityName = "";
+            let activityArray = [];
+            let activityArrayIndex = -1;
+            
+            for (let i = 0; i < data.length; i++) {
+                let activity = data[i];
+                let activityName = activity.activity;
 
-// get single item by id on slide 11 in cRUD ops
+                if (activityName === prevActivityName) {
+                    activityArray[activityArrayIndex].option.push(activity.option)
+                    activityArray[activityArrayIndex].carbon.push(activity.carbon)
+                } else {
+                    let activityObject = {
+                        "id": activity.id,
+                        "activity": activity.activity,
+                        "category": activity.category,
+                        "qty": activity.qty,
+                        "unit": activity.unit,
+                        "option": [activity.option],
+                        "carbon": [activity.carbon],
+                        "pollutants": activity.pollutants,
+                        "land": activity.land,
+                        "water": activity.water,
+                        "yearly_figure": activity.yearly_figure,
+                        "notes": activity.notes,
+                        "created_at": activity.created_at
+                    }
+                    activityArray.push(activityObject)
+                    activityArrayIndex += 1;
+                    prevActivityName = activityName
+                }
+                // console.log(activityArray)
+            }
+            res.status(200).json(activityArray);
+        })
+        .catch((err) =>
+            res.status(400).send(`Error retrieving activities: ${err}`)
+        );
+};
